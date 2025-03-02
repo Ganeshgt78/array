60a09b578072d8e6dcf92bb62cce6e674a0299ba
ganesh-g-t <ganeshgtganesh78@gmail.com>
2025-03-02 20:27:08 +0530

    initial commit for this project

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
-
     public static void main(String[] args) {
-        int arr1[][]=new int[3][2];
-        int arr2[][]=new int[3][2];
-        int arr3[][]=new int[3][2];
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the arr1");
-        for (int i=0;i<3;i++){
-            for(int j=0;j<2;j++){
-                arr1[i][j]=sc.nextInt();
-
-            }
-        }
-        System.out.println("enter the arr2");
-        for (int l=0;l<3;l++) {
-            for (int m = 0; m < 2; m++) {
-                arr2[l][m] = sc.nextInt();
-
-            }
-        }
-        System.out.println("enter the arr3");
-        for (int l=0;l<3;l++) {
-            for (int m =0; m <2; m++) {
-                arr3[l][m] = arr1[l][m] + arr2[l][m];
-                System.out.println(arr3[l][m]+" ");
-            }
-            System.out.println();
-        }
+       Stack<String>stack=new Stack<String>();
+        System.out.println(stack.empty());
+       stack.push("item");
+       stack.push("bag");
+       stack.push("college");
+       stack.push("school");
+        System.out.println(stack.peek());
+        System.out.println(stack.search("bag"));
+        String MyfavGame = stack.pop();
+        System.out.println(MyfavGame);
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.lastElement());
     }
+}
 
-        }
